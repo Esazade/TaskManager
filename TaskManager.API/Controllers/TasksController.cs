b84f7ba06ed24c6aa5d9761b867ab68387a82a79
leyla esazade <esazadejob@gmail.com>
2025-03-21 19:27:11 +0330

    Add Task controller

---
+++ TaskManager.API/Controllers/TasksController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Application.Interfaces;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TasksController : ControllerBase
+{
+    private readonly ITaskRepository _taskRepository;
+
+    public TasksController(ITaskRepository taskRepository)
+    {
+        _taskRepository = taskRepository;
+    }
+
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TaskItem>>> GetTasks()
+    {
+        var tasks = await _taskRepository.GetAllAsync();
+        return Ok(tasks);
+    }
+
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TaskItem>> GetTask(int id)
+    {
+        var task = await _taskRepository.GetByIdAsync(id);
+        if (task == null)
+            return NotFound();
+
+        return Ok(task);
+    }
+
+   
+    [HttpPost]
+    public async Task<ActionResult<TaskItem>> CreateTask(TaskItem task)
+    {
+        await _taskRepository.AddAsync(task);
+        return CreatedAtAction(nameof(GetTask), new { id = task.Id }, task);
+    }
+
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateTask(int id, TaskItem task)
+    {
+        if (id != task.Id)
+            return BadRequest();
+
+        await _taskRepository.UpdateAsync(task);
+        return NoContent();
+    }
+
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTask(int id)
+    {
+        var task = await _taskRepository.GetByIdAsync(id);
+        if (task == null)
+            return NotFound();
+
+        await _taskRepository.DeleteAsync(task);
+        return NoContent();
+    }
+}
--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Application.Interfaces;
+using TaskManager.Infrastructure.Persistence;
+using TaskManager.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
 // Add services to the container.
 

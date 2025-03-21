cfaeb20e7478004a12a594465a82878d72bcda94
leyla esazade <esazadejob@gmail.com>
2025-03-21 14:41:22 +0330

    Add Task repository

---
+++ TaskManager.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Application.Interfaces;
+using TaskManager.Domain.Entities;
+using TaskManager.Infrastructure.Persistence;
+
+namespace TaskManager.Infrastructure.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TaskRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TaskItem>> GetAllAsync()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(int id)
+        {
+            return await _context.Tasks.FindAsync(id);
+        }
+
+        public async Task AddAsync(TaskItem task)
+        {
+            await _context.Tasks.AddAsync(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TaskItem task)
+        {
+            _context.Tasks.Update(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TaskItem task)
+        {
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
